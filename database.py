7ac92eeb2a98d698d8930efb7e16330dc9daf61e
ayush-pandey047 <23417dp@gmail.com>
2026-06-02 04:30:51 +0530

    sample data insert

--- database.py
+++ database.py
@@ -60,3 +60,31 @@ def setup_database():
         ]
     }
 
+    for table_name, columns in sample_table.items():
+        cols_def = ",".join([f"{col}{dtype}" for col, dtype in columns])
+        cursor.execute(f"CREATE TABLE IF NOT EXISTS {table_name} ({cols_def})")
+
+        for col, dtype in columns:
+            cusour.execute("""
+                INSERT INTO schema_info (table_name, column_name, column_type, db_id)
+                VALUES (?, ?, ?, ?)
+            """, (table_name, col, dtype, 'university'))
+
+    
+    cursor.executemany("INSERT OR IGNORE INTO departments VALUES (?,?,?,?)", [
+        (1, "Computer Science", "Tech Block", 500000),
+        (2, "Mathematics", "Science Block", 300000),
+        (3, "Physics", "Science Block", 250000),
+    ])
+    
+    cursor.executemany("INSERT OR IGNORE INTO enrollments VALUES (?,?,?,?,?)", [
+        (1, 101, 1, 150, "Fall2024"),
+        (2, 102, 2, 80, "Fall2024"),
+        (3, 103, 1, 120, "Spring2024"),
+    ])
+    
+    cursor.executemany("INSERT OR IGNORE INTO courses VALUES (?,?,?,?,?)", [
+        (1, "Data Structures", 1, 0, 4),
+        (2, "Linear Algebra", 2, 1, 3),
+        (3, "Quantum Physics", 3, 0, 4),
+    ])
